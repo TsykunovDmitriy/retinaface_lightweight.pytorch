c17ede006684673596a44cb0f79d185e4a3bd9e0
Tsykunov Dmitriy <tsykunov@photolab.me>
2020-10-15 13:05:21 +0000

    refactoring output format

--- detector.py
+++ detector.py
@@ -73,9 +73,11 @@ class RetinaDetector:
         dets = dets[keep, :]
         landms = landms[keep]
 
-        dets = np.concatenate((dets, landms), axis=1)
+        boxes = dets[:, :4]
+        scores = dets[:, 4]
+        landms = landms.reshape(-1, 5, 2)
 
-        return dets
+        return boxes, landms, scores
 
 if __name__ == "__main__":
     from utils import video_utils
@@ -86,23 +88,22 @@ if __name__ == "__main__":
     video, audio_path, fps = video_utils.vidread("./test_video.mp4")
     output = []
     for frame in tqdm(video):
-        dets = detector(frame)
+        boxes, landms, scores = detector(frame)
 
-        for b in dets:
-            text = "{:.4f}".format(b[4])
-            cv2.rectangle(frame, (int(b[0]), int(b[1])), (int(b[2]), int(b[3])), (0, 0, 255), 2)
-            cx = b[0]
-            cy = b[1] + 12
+        for i in range(len(boxes)):
+            text = "{:.4f}".format(scores[i])
+            cv2.rectangle(frame, (int(boxes[i][0]), int(boxes[i][1])), (int(boxes[i][2]), int(boxes[i][3])), (0, 0, 255), 2)
+            cx = boxes[i][0]
+            cy = boxes[i][1] + 12
             cv2.putText(frame, text, (int(cx), int(cy)),
                         cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 255))
 
-            land = b[5:].reshape(5, 2)
             # landms
-            cv2.circle(frame, (land[0][0], land[0][1]), 1, (0, 0, 255), 4)
-            cv2.circle(frame, (land[1][0], land[1][1]), 1, (0, 255, 255), 4)
-            cv2.circle(frame, (land[2][0], land[2][1]), 1, (255, 0, 255), 4)
-            cv2.circle(frame, (land[3][0], land[3][1]), 1, (0, 255, 0), 4)
-            cv2.circle(frame, (land[4][0], land[4][1]), 1, (255, 0, 0), 4)
+            cv2.circle(frame, (landms[i][0][0], landms[i][0][1]), 1, (0, 0, 255), 4)
+            cv2.circle(frame, (landms[i][1][0], landms[i][1][1]), 1, (0, 255, 255), 4)
+            cv2.circle(frame, (landms[i][2][0], landms[i][2][1]), 1, (255, 0, 255), 4)
+            cv2.circle(frame, (landms[i][3][0], landms[i][3][1]), 1, (0, 255, 0), 4)
+            cv2.circle(frame, (landms[i][4][0], landms[i][4][1]), 1, (255, 0, 0), 4)
         
         output.append(frame[..., ::-1])
 
